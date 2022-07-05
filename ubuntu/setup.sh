b78bd3b3f5bccc08d2148faa4f6444b9a5aa8df9
Yuzu <3068107+yuzupup@users.noreply.github.com>
2022-07-05 12:45:26 -0400

    Update setup.sh

--- ubuntu/setup.sh
+++ ubuntu/setup.sh
@@ -5,6 +5,8 @@ sudo apt-get update
 # common utils
 # fzy: fuzzy search for terminal
 sudo apt-get install fzy
+# neovim: needs no intro
+sudo apt-get install neovim
 
 # zsh
 sudo apg-get install zsh
